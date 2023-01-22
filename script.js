fbf5159d0181bfe0c9734eae436e3ab4b0ac3944
Annisworld <115580728+Annisworld@users.noreply.github.com>
2023-01-22 11:35:50 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,9 @@ function generate(){
      "- Harriet Styles" : '"I\'d walk through fire for you, just let me adore you... OKAY!"',
      "- HS" : '"To boyfriends everywhere...FUCK YOU"',
      "- Louis\'s boo" : '"I like girls!"',
-     "- Sunshine" : '"Sweet creature, sweet creature, when I run out of road, you bring me home, You\'ll bring me home"'
-  }
+     "- Sunshine" : '"Sweet creature, sweet creature, when I run out of road, you bring me home, You\'ll bring me home"'}
   let authors = Object.keys(quotes);
   let author = authors[Math.floor(Math.random() * authors.length)];
   let quote = quotes[author];
-  
   document.getElementById("author").innerHTML = author;
   document.getElementById("quote").innerHTML = quote;}

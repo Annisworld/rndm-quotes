86dadb648f0216840ba70dfc498f859a1415d91d
Annisworld <115580728+Annisworld@users.noreply.github.com>
2023-04-17 22:20:52 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function generate(){
   let quotes = {
-     "- Harry Styles" : '"It\'s a family show!... or is it?"',
-     "- Harrold Styles" : '"Tina, She\'s GAY!"',
-     "- Harriet Styles" : '"I\'d walk through fire for you, just let me adore you... OKAY!"',
-     "- HS" : '"To boyfriends everywhere...FUCK YOU"',
-     "- Louis\'s boo" : '"I like girls!"',
-     "- Sunshine" : '"Sweet creature, sweet creature, when I run out of road, you bring me home, You\'ll bring me home"'
+     "- Sue Murphy" : '"Did you ever walk into a room and forget why you walked in? I think that\'s how dogs spend their lives."',
+     "- Devin J. Monroe" : '"The only thing we have to fear is fear itself, but raptors are pretty dang scary."',
+     "- Edith Wharton" : '"The only way not to think about money is to have a great deal of it."',
+     "- Paula Poundstone" : '"Adults are always asking children what they want to be when they grow up because they\'re looking for ideas."',
+     "- Phyllis Diller" : '"I want my children to have all the things I couldn’t afford. Then I want to move in with them."',
+     "- Anonymous" : '"Friends are God\'s way of apologizing to us for our families."'
   }
   let authors = Object.keys(quotes);
   let author = authors[Math.floor(Math.random() * authors.length)];
